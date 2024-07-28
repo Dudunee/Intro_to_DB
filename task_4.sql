95f30cbc7e066d97dab02d78498d63f5989c4790
Dudune <yahyabakhouchh@gmail.com>
2024-07-28 14:00:55 -0700

    add task_4

--- task_4.sql
+++ task_4.sql
@@ -2,8 +2,7 @@ USE alx_book_store;
 
 SELECT 
     COLUMN_NAME AS 'Column Name',
-    DATA_TYPE AS 'Data Type',
-    CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+    COLUMN_TYPE AS 'Column Type',
     IS_NULLABLE AS 'Nullable',
     COLUMN_DEFAULT AS 'Default Value'
 FROM 
@@ -11,3 +10,4 @@ FROM
 WHERE 
     TABLE_SCHEMA = 'alx_book_store' AND 
     TABLE_NAME = 'Books';
+
